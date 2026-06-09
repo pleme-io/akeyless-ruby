cb725969aa9f0350d4c67ebc154f9ddbc18b2a02
drzzln <drzzln@protonmail.com>
2026-06-08 17:26:46 -0700

    flake: follow substrate/nixpkgs (fleet nixpkgs alignment for cache reuse)

--- flake.nix
+++ flake.nix
@@ -2,10 +2,9 @@
   description = "Akeyless Ruby SDK - auto-generated Ruby gem for the Akeyless API";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.follows = "substrate/nixpkgs";
     substrate = {
       url = "github:pleme-io/substrate";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
   };
